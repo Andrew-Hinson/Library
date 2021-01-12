b45d97197696e86412db1a60ca0659afb7ca23e6
LivingWill <william.hinson@outlook.com>
2021-01-11 18:49:15 -0600

    read radio button toggles icon, is dynamically set from form

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const titleSubmit = document.querySelector('#titleSubmit')
 const bookForm = document.querySelector('#bookForm')
 const displayLib = document.querySelector('#displayContainer')
 let myLibrary = []
-
 function Book (title, author, pages, read) {
     //constructor
     this.title = title
@@ -42,16 +41,25 @@ function addBookToLibrary() {
     myLibrary.push(work)
 }
 
+
+
 function addElement() {
     const lastIndex = myLibrary.length - 1;
         myLibrary.map((book, i) => {
         if(i === lastIndex){
         let newDiv = document.createElement('div');
-        let newP = document.createElement('p');
         let deleteBtn = document.createElement('button')
-        // deleteBtn.addEventListener('click', curDiv => 0.
-        newP.innerText = `${book.title} ${book.author} ${book.pages} ${book.read}`
-
+        let p1 = document.createElement('p');
+        let p2 = document.createElement('p');
+        let p3 = document.createElement('p');
+        let p4 = document.createElement('p');
+        let icon = document.createElement('i')
+        let radio1 = document.createElement('input');
+        let radio2 = document.createElement('input');
+        let label1 = document.createElement('label');
+        let label2 = document.createElement('label');
+        
+        /////////////////////////////
         deleteBtn.classList.add('delete');
         deleteBtn.innerText = 'Delete';
         deleteBtn.value = 'delete';
@@ -59,29 +67,67 @@ function addElement() {
         //////////////////////////////
         newDiv.dataset.index = i;
         newDiv.classList.add('card')
+        //////////////////////////////
+        radio1.setAttribute('type', 'radio');
+        radio1.setAttribute('name', `${i}`);
+        radio1.setAttribute('value', '0')
+        radio2.setAttribute('type', 'radio');
+        radio2.setAttribute('value', '1')
+        radio2.setAttribute('name', `${i}`);
+        if(book.read == '0'){
+          radio1.setAttribute('checked','checked')
+          icon.classList.add('fas','fa-book-reader', 'fa-2x')
+        } else {
+          radio2.setAttribute('checked','checked')
+          icon.classList.add('fas', 'fa-book', 'fa-2x')
+        }
+        label1.innerText = ('Read:');
+        label2.innerText = (' Not Read:');
         
+        //////////////////////////////
+        
+        
+        icon.setAttribute('id', `icon${i}`);
+        
+        p1.innerText = `Title: ${book.title}`
+        p2.innerText = `Author: ${book.author}`
+        p3.innerText = `Pages: ${book.pages}`
+        
+        p4.appendChild(label1);
+        p4.appendChild(radio1);
+        p4.appendChild(label2);
+        p4.appendChild(radio2);
         displayLib.appendChild(newDiv);
-        newDiv.appendChild(newP);
-        newP.appendChild(deleteBtn);
-
-        
-
+        newDiv.appendChild(icon);
+        newDiv.appendChild(p1);
+        newDiv.appendChild(p2);
+        newDiv.appendChild(p3);
+        newDiv.appendChild(p4);
+        newDiv.appendChild(deleteBtn);
         }
     })
 }
 
 //delete whatever the dataset index is 5head
 displayLib.addEventListener('click', (e) => {
-  let card = document.querySelector('card');
   let target = e.target
+  
   if(target.value == 'delete'){
     let i = target.dataset.index;
-    let removeDiv = document.querySelector(`[data-index='${i}']`)
-    displayLib.removeChild(removeDiv)
+    let targetedDiv = document.querySelector(`[data-index='${i}']`)
+    displayLib.removeChild(targetedDiv)
     }
-   else
-    console.log('it does not work')
-  })
+  if(target.type == 'radio'){
+    let y = target.name
+    let iconChange = document.querySelector(`#icon${y}`)
+    if(target.value == '0'){
+      iconChange.classList.add('fas','fa-book-reader', 'fa-2x')
+    } else {
+      iconChange.classList.remove('fas','fa-book-reader', 'fa-2x')
+      iconChange.classList.add('fas', 'fa-book', 'fa-2x')
+    }
+  } 
+})
 
 
 //click event grabs data
